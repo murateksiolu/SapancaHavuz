9d6c7c6078d98fb8cebfa12dd66abaf26d4289d6
murateksiolu <murateksicom@gmail.com>
2023-12-11 22:59:29 +0300

    commit

--- external/form/request-service-form.php
+++ external/form/request-service-form.php
@@ -11,7 +11,7 @@ require 'phpmailer/SMTP.php';
 
 //Create an instance; passing `true` enables exceptions
 $mail = new PHPMailer(true);
-$file = $_FILES['file'];
+// $file = $_FILES['file'];
 
 try {
     //Server settings
@@ -19,29 +19,29 @@ try {
     $mail->isSMTP();                                            //Send using SMTP
     $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'mailwebsmirno@gmail.com';                     //SMTP username
-    $mail->Password   = 'mail0web_smirnoQ';                               //SMTP password
-    $mail->SMTPSecure = "ssl";            //Enable implicit TLS encryption
-    $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+    $mail->Username   = 'no-reply@eracochillers.com';                     //SMTP username
+    $mail->Password   = '314159Ea!';                               //SMTP password
+    $mail->SMTPSecure = "PHPMailer::ENCRYPTION_STARTTLS";            //Enable implicit TLS encryption
+    $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
     //Recipients
-    $mail->setFrom('mailwebsmirno@gmail.com', 'Pool');
-    $mail->addAddress('youremail@gmail.com', 'Pool');     //Add a recipient
+    $mail->setFrom('no-reply@eracochillers.com', 'Pool');
+    $mail->addAddress('murateksicom@gmail.com', 'Pool');     //Add a recipient
 
     //Attachments
     // $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
     // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
-    if (!empty($file['name'][0])) {
-        for ($ct = 0; $ct < count($file['tmp_name']); $ct++){
-            $uploadfile = tempnam(sys_get_temp_dir(), sha1($file['name'][$ct]));
-            $filename = $file['name'][$ct];
-            if (move_uploaded_file($file['tmp_name'][$ct], $uploadfile)){
-                $mail->addAttachment($uploadfile, $filename);
-                $rfile[] = "File $filename attached";
-            } else {
-                $rfile[] = "Failed to attach file $filename";
-            }
-        }
+    // if (!empty($file['name'][0])) {
+    //     for ($ct = 0; $ct < count($file['tmp_name']); $ct++){
+    //         $uploadfile = tempnam(sys_get_temp_dir(), sha1($file['name'][$ct]));
+    //         $filename = $file['name'][$ct];
+    //         if (move_uploaded_file($file['tmp_name'][$ct], $uploadfile)){
+    //             $mail->addAttachment($uploadfile, $filename);
+    //             $rfile[] = "File $filename attached";
+    //         } else {
+    //             $rfile[] = "Failed to attach file $filename";
+    //         }
+    //     }
     };
 
     //Passed variables
